918583f97bcba5553d8282f711340f1d8ca1d230
jyj <yu_jie_jing@163.com>
2020-03-18 10:17:59 +0800

    更新1

---
+++ src/cn/edu/shzc/Hello.java
@@ -0,0 +1,14 @@
+package cn.edu.shzc;
+
+/*
+ * @Description
+ * @author 景玉洁
+ * @date 2020年3月18日 上午10:11:36
+ */
+public class Hello
+{
+	public static void main(String[] args)
+	{
+		System.out.println("hello");
+	}
+}
